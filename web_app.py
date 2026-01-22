7fb8877ffba3e4a4c9c30e777d99412832ff8e2e
uday29vaja <137177224+uday29vaja@users.noreply.github.com>
2026-01-22 11:08:28 +0530

    5 years beta

--- web_app.py
+++ web_app.py
@@ -21,7 +21,7 @@ from zeep import Client, Transport
 # Config
 # -------------------------------
 END_DATE = datetime.now(timezone.utc).date()
-START_DATE = END_DATE - timedelta(days=365)
+START_DATE = END_DATE - timedelta(days=365*5)
 YAHOO_INDEX_TICKER = "^NSEI"
 Access_Token="A9fK3M2ZQ7xP4R8W"
 # Create logs directory
@@ -435,7 +435,7 @@ def get_benchmark_data():
         
     try:
         end_date = datetime.now(timezone.utc)
-        start_date = end_date - timedelta(days=365)  # 1 years
+        start_date = end_date - timedelta(days=365*5)  # 5 years
         
         print("📈 Downloading Nifty 50 benchmark data...")
         nifty_data = yf.download(
@@ -612,9 +612,9 @@ def nse_symbols_cached():
     return Get_EQSymbol()  
 
 
-# @st.cache_data
-# def BSE_symbols_cached():
-#     return Get_BSESymbol()  
+@st.cache_data
+def BSE_symbols_cached():
+    return Get_BSESymbol()  
 # -------------------------------
 # Page Setup
 # -------------------------------
